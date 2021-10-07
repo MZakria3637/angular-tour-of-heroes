b391c62d4842f7e14cb04e01cb6dffdf2ae7712e
MZakria3637 <zakriamuhammad3637@gmail.com>
2021-10-07 17:05:40 +0500

    last commit

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -3,7 +3,7 @@ import { InMemoryDbService } from 'angular-in-memory-web-api';
 import { Hero } from './hero';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class InMemoryDataService implements InMemoryDbService
 {
@@ -21,12 +21,10 @@ export class InMemoryDataService implements InMemoryDbService
       { id: 19, name: 'Magma' },
       { id: 20, name: 'Tornado' }
     ];
-    return heroes;
+    return { heroes };
   }
   genId(heroes: Hero[]): number
   {
     return heroes.length > 0 ? Math.max(...heroes.map(hero => hero.id)) + 1 : 11;
   }
-
- 
 }
